f4bf4f37abc99a9b3eae588db5462d266d5e95be
fhgaha <antsy1992@gmail.com>
2022-09-29 09:48:30 +0300

    scrollable tickets on windows

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:booktickets/screens/ticket_view.dart';
 import 'package:booktickets/utils/app_styles.dart';
+import 'package:booktickets/utils/my_custom_scroll_behaviour.dart';
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
@@ -9,90 +10,104 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Styles.bgColor,
-      body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        children: [
-          Container(
-            //book tickets and incoming flights
-            child: Column(
-              children: [
-                const Gap(40),
-                //good morning, book tickets, image
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    //good morning, book tickets
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Good morning", style: Styles.headLineStyle3),
-                        const Gap(5),
-                        Text("Book Tickets", style: Styles.headLineStyle1),
-                      ],
-                    ),
-                    // img
-                    Container(
-                      width: 50,
-                      height: 50,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: const DecorationImage(
-                          fit: BoxFit.cover,
-                          image: AssetImage("assets/images/img_1.png"),
-                        ),
-                      ),
-                      child: null,
-                    ),
-                  ],
-                ),
-                const Gap(25),
-                //search bar
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: const Color(0xfff4f6fd),
-                  ),
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-                  child: Row(
+    return MaterialApp(
+      scrollBehavior: MyCustomScrollBehavior(),
+      home: Scaffold(
+        backgroundColor: Styles.bgColor,
+        body: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          children: [
+            Container(
+              //book tickets and incoming flights
+              child: Column(
+                children: [
+                  const Gap(40),
+                  //good morning, book tickets, image
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Icon(
-                        FluentSystemIcons.ic_fluent_search_regular,
-                        color: Colors.grey.shade500,
+                      //good morning, book tickets
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Good morning", style: Styles.headLineStyle3),
+                          const Gap(5),
+                          Text("Book Tickets", style: Styles.headLineStyle1),
+                        ],
                       ),
-                      Text(
-                        "Search",
-                        style: Styles.headLineStyle4,
+                      // img
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: const DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage("assets/images/img_1.png"),
+                          ),
+                        ),
+                        child: null,
                       ),
                     ],
                   ),
-                ),
-                const Gap(40),
-                //upcomng flights
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Upcoming Flights", style: Styles.headLineStyle2),
-                    InkWell(
-                      onTap: () {
-                        print("You are tapped " "View all" "");
-                      },
-                      child: Text(
-                        "View all",
-                        style: Styles.textStyle
-                            .copyWith(color: Styles.primaryColor),
-                      ),
+                  const Gap(25),
+                  //search bar
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color(0xfff4f6fd),
                     ),
-                  ],
-                ),
-              ],
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 12, vertical: 12),
+                    child: Row(
+                      children: [
+                        Icon(
+                          FluentSystemIcons.ic_fluent_search_regular,
+                          color: Colors.grey.shade500,
+                        ),
+                        Text(
+                          "Search",
+                          style: Styles.headLineStyle4,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Gap(40),
+                  //upcomng flights
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Upcoming Flights", style: Styles.headLineStyle2),
+                      InkWell(
+                        onTap: () {
+                          print("You are tapped " "View all" "");
+                        },
+                        child: Text(
+                          "View all",
+                          style: Styles.textStyle
+                              .copyWith(color: Styles.primaryColor),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-          const Gap(15),
-          TicketView(),
-        ],
+            const Gap(15),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  TicketView(),
+                  TicketView(),
+                ],
+              ),
+            )
+            // MaterialApp(
+            //   scrollBehavior: MyCustomScrollBehavior(),
+            // ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/utils/my_custom_scroll_behaviour.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class MyCustomScrollBehavior extends MaterialScrollBehavior {
+  // Override behavior methods like buildOverscrollIndicator and buildScrollbar
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+        // etc.
+      };
+}
