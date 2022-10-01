8d0ba3b4b7f9a9bbc9a89bc64f95e14c43806754
fhgaha <antsy1992@gmail.com>
2022-10-01 07:48:24 +0300

    profile screen new award

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -21,6 +21,7 @@ class ProfileScreen extends StatelessWidget {
         children: [
           Gap(AppLayout.getHeight(40)),
           Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               //img
               Container(
@@ -35,8 +36,8 @@ class ProfileScreen extends StatelessWidget {
                   ),
                 ),
               ),
-              //text column
               Gap(AppLayout.getHeight(10)),
+              //text column
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.start,
@@ -91,6 +92,95 @@ class ProfileScreen extends StatelessWidget {
                     ),
                   ),
                 ],
+              ),
+              const Spacer(),
+              //edit
+              Column(
+                children: [
+                  InkWell(
+                    onTap: () => print("you tabbed edit on profile screen"),
+                    child: Text(
+                      "Edit",
+                      style: Styles.textStyle.copyWith(
+                        color: Styles.primaryColor,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Gap(AppLayout.getHeight(8)),
+          Divider(color: Colors.grey.shade300),
+          //youve got new award
+          Stack(
+            children: [
+              //blue area
+              Container(
+                height: AppLayout.getHeight(90),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Styles.primaryColor,
+                  borderRadius: BorderRadius.circular(AppLayout.getHeight(18)),
+                ),
+              ),
+              //blue ring
+              Positioned(
+                right: -45,
+                top: -40,
+                child: Container(
+                  padding: EdgeInsets.all(AppLayout.getHeight(30)),
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    shape: BoxShape.circle,
+                    border:
+                        Border.all(width: 18, color: const Color(0xff264cd2)),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: AppLayout.getHeight(25),
+                  vertical: AppLayout.getHeight(15),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    //lightbulb
+                    CircleAvatar(
+                      radius: 25,
+                      backgroundColor: Colors.white,
+                      child: Icon(
+                        FluentSystemIcons.ic_fluent_lightbulb_filament_filled,
+                        color: Styles.primaryColor,
+                        size: 27,
+                      ),
+                    ),
+                    Gap(AppLayout.getHeight(10)),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "You've got a new reward",
+                          style: Styles.headLineStyle2.copyWith(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          "You have 95 flights in a year",
+                          style: Styles.headLineStyle2.copyWith(
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white.withOpacity(0.9),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
